bf6bcae922056ce9cb2fed7b9d7ae1db3a71435e
Baiocchi Jose Ignacio <baiocchi73@gmail.com>
2023-10-03 15:54:18 -0300

    cambios

--- src/ProjectoFinalSalud/Vistas/Inicio.java
+++ src/ProjectoFinalSalud/Vistas/Inicio.java
@@ -1,17 +1,11 @@
 
 package ProjectoFinalSalud.Vistas;
 
-import java.awt.event.MouseWheelListener;
-
 public class Inicio extends javax.swing.JFrame {
         
     public Inicio() {
         
-        initComponents();
-        textoAreaActivos.setVisible(false);
-        textoAreaEspecial.setVisible(false);
-        textoAreaOrden.setVisible(false);
-        
+        initComponents();     
         
     }
 
@@ -27,18 +21,9 @@ public class Inicio extends javax.swing.JFrame {
         etiquetaNumDeEmerg = new javax.swing.JLabel();
         botonUsuario = new javax.swing.JButton();
         botonActivos = new javax.swing.JButton();
-        jScrollPane2 = new javax.swing.JScrollPane();
-        textoAreaActivos = new javax.swing.JTextArea();
         botonEspecial = new javax.swing.JButton();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        textoAreaEspecial = new javax.swing.JTextArea();
         botonOrden = new javax.swing.JButton();
-        jScrollPane3 = new javax.swing.JScrollPane();
-        textoAreaOrden = new javax.swing.JTextArea();
         jScrollBar1 = new javax.swing.JScrollBar();
-        etiquetaBanners = new javax.swing.JLabel();
-        etiquetaBannerAbajo = new javax.swing.JLabel();
-        etiquetaUltimo = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(255, 255, 255));
@@ -76,81 +61,33 @@ public class Inicio extends javax.swing.JFrame {
         botonActivos.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ImagenesDelProyecto/Activos.png"))); // NOI18N
         botonActivos.setText("Afiliados y Prestadores Activos");
         botonActivos.setHorizontalTextPosition(javax.swing.SwingConstants.LEFT);
-        botonActivos.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseEntered(java.awt.event.MouseEvent evt) {
-                botonActivosMouseEntered(evt);
-            }
-            public void mouseExited(java.awt.event.MouseEvent evt) {
-                botonActivosMouseExited(evt);
-            }
-        });
         botonActivos.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 botonActivosActionPerformed(evt);
             }
         });
 
-        textoAreaActivos.setColumns(20);
-        textoAreaActivos.setRows(5);
-        textoAreaActivos.setText("Consulta nuestra \nlista de afiliados \ny prestadores \nactivos");
-        jScrollPane2.setViewportView(textoAreaActivos);
-
         botonEspecial.setBackground(new java.awt.Color(255, 255, 255));
         botonEspecial.setFont(new java.awt.Font("Calibri", 0, 11)); // NOI18N
         botonEspecial.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ImagenesDelProyecto/Especialidades.png"))); // NOI18N
         botonEspecial.setText("Especialidades");
         botonEspecial.setHorizontalTextPosition(javax.swing.SwingConstants.LEFT);
-        botonEspecial.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseEntered(java.awt.event.MouseEvent evt) {
-                botonEspecialMouseEntered(evt);
-            }
-            public void mouseExited(java.awt.event.MouseEvent evt) {
-                botonEspecialMouseExited(evt);
-            }
-        });
         botonEspecial.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 botonEspecialActionPerformed(evt);
             }
         });
 
-        textoAreaEspecial.setEditable(false);
-        textoAreaEspecial.setColumns(20);
-        textoAreaEspecial.setFont(new java.awt.Font("Calibri", 0, 13)); // NOI18N
-        textoAreaEspecial.setRows(5);
-        textoAreaEspecial.setText("Consulta que especialidades \ntenemos disponibles \npara vos !");
-        jScrollPane1.setViewportView(textoAreaEspecial);
-
         botonOrden.setBackground(new java.awt.Color(255, 255, 255));
         botonOrden.setFont(new java.awt.Font("Calibri", 0, 11)); // NOI18N
         botonOrden.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ImagenesDelProyecto/Ordenes.png"))); // NOI18N
         botonOrden.setText("Ordenes De Consulta");
         botonOrden.setHorizontalTextPosition(javax.swing.SwingConstants.LEFT);
-        botonOrden.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseEntered(java.awt.event.MouseEvent evt) {
-                botonOrdenMouseEntered(evt);
-            }
-            public void mouseExited(java.awt.event.MouseEvent evt) {
-                botonOrdenMouseExited(evt);
-            }
-        });
         botonOrden.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 botonOrdenActionPerformed(evt);
             }
         });
-
-        textoAreaOrden.setColumns(20);
-        textoAreaOrden.setRows(5);
-        textoAreaOrden.setText("Tus ordenes de consulta\ndispuestas según tus\nnecesidades !");
-        jScrollPane3.setViewportView(textoAreaOrden);
-
-        etiquetaBanners.setBackground(new java.awt.Color(255, 255, 255));
-        etiquetaBanners.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ImagenesDelProyecto/baners informativos.png"))); // NOI18N
-
-        etiquetaBannerAbajo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ImagenesDelProyecto/BannerDeAbajo.png"))); // NOI18N
-
-        etiquetaUltimo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ImagenesDelProyecto/Vista De Trabajo 0--arreglarrr.png"))); // NOI18N
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -173,35 +110,15 @@ public class Inicio extends javax.swing.JFrame {
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(96, 96, 96)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addComponent(etiquetaBannerAbajo, javax.swing.GroupLayout.PREFERRED_SIZE, 895, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addComponent(botonActivos, javax.swing.GroupLayout.PREFERRED_SIZE, 257, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(134, 134, 134)
-                                .addComponent(botonEspecial)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addComponent(botonOrden, javax.swing.GroupLayout.PREFERRED_SIZE, 199, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(148, 148, 148))))
+                        .addComponent(botonActivos, javax.swing.GroupLayout.PREFERRED_SIZE, 257, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(134, 134, 134)
+                        .addComponent(botonEspecial)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(botonOrden, javax.swing.GroupLayout.PREFERRED_SIZE, 199, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(148, 148, 148))
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addGap(135, 135, 135)
-                                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(143, 143, 143)
-                                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 241, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(124, 124, 124)
-                                .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 242, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addContainerGap()
-                                .addComponent(etiquetaPubli))
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addGap(113, 113, 113)
-                                .addComponent(etiquetaBanners, javax.swing.GroupLayout.PREFERRED_SIZE, 906, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addGap(31, 31, 31)
-                                .addComponent(etiquetaUltimo, javax.swing.GroupLayout.PREFERRED_SIZE, 903, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addContainerGap()
+                        .addComponent(etiquetaPubli)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 36, Short.MAX_VALUE)))
                 .addComponent(jScrollBar1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
@@ -226,20 +143,7 @@ public class Inicio extends javax.swing.JFrame {
                             .addComponent(botonActivos)
                             .addComponent(botonEspecial, javax.swing.GroupLayout.PREFERRED_SIZE, 67, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(botonOrden, javax.swing.GroupLayout.PREFERRED_SIZE, 67, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addGap(5, 5, 5)
-                                .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                        .addGap(18, 18, 18)
-                        .addComponent(etiquetaBanners, javax.swing.GroupLayout.PREFERRED_SIZE, 194, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(18, 18, 18)
-                        .addComponent(etiquetaBannerAbajo, javax.swing.GroupLayout.PREFERRED_SIZE, 224, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(etiquetaUltimo, javax.swing.GroupLayout.PREFERRED_SIZE, 191, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(0, 82, Short.MAX_VALUE)))
+                        .addGap(0, 565, Short.MAX_VALUE)))
                 .addContainerGap())
         );
 
@@ -253,7 +157,9 @@ public class Inicio extends javax.swing.JFrame {
         );
         panelEscritorioLayout.setVerticalGroup(
             panelEscritorioLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(panelEscritorioLayout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
 
         getContentPane().add(panelEscritorio, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
@@ -268,41 +174,17 @@ public class Inicio extends javax.swing.JFrame {
         orden.setVisible(true);
     }//GEN-LAST:event_botonOrdenActionPerformed
 
-    private void botonOrdenMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_botonOrdenMouseExited
-        textoAreaOrden.setVisible(false);
-    }//GEN-LAST:event_botonOrdenMouseExited
-
-    private void botonOrdenMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_botonOrdenMouseEntered
-        textoAreaOrden.setVisible(true);
-    }//GEN-LAST:event_botonOrdenMouseEntered
-
     private void botonEspecialActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonEspecialActionPerformed
         Especialidades esp = new Especialidades();
         panelEscritorio.add(esp);
         esp.setVisible(true);
     }//GEN-LAST:event_botonEspecialActionPerformed
 
-    private void botonEspecialMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_botonEspecialMouseExited
-        textoAreaEspecial.setVisible(false);
-    }//GEN-LAST:event_botonEspecialMouseExited
-
-    private void botonEspecialMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_botonEspecialMouseEntered
-        textoAreaEspecial.setVisible(true);
-    }//GEN-LAST:event_botonEspecialMouseEntered
-
     private void botonActivosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonActivosActionPerformed
         AfiliadosPrestadoresActivos activos = new AfiliadosPrestadoresActivos();
         panelEscritorio.add(activos);
         activos.setVisible(true);
     }//GEN-LAST:event_botonActivosActionPerformed
-
-    private void botonActivosMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_botonActivosMouseExited
-        textoAreaActivos.setVisible(false);
-    }//GEN-LAST:event_botonActivosMouseExited
-
-    private void botonActivosMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_botonActivosMouseEntered
-        textoAreaActivos.setVisible(true);
-    }//GEN-LAST:event_botonActivosMouseEntered
 
     private void botonUsuarioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonUsuarioActionPerformed
         Cuentas cuentas = new Cuentas();
@@ -350,21 +232,12 @@ public class Inicio extends javax.swing.JFrame {
     private javax.swing.JButton botonEspecial;
     private javax.swing.JButton botonOrden;
     private javax.swing.JButton botonUsuario;
-    private javax.swing.JLabel etiquetaBannerAbajo;
-    private javax.swing.JLabel etiquetaBanners;
     private javax.swing.JLabel etiquetaLogo;
     private javax.swing.JLabel etiquetaNumDeEmerg;
     private javax.swing.JLabel etiquetaPubli;
-    private javax.swing.JLabel etiquetaUltimo;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollBar jScrollBar1;
-    private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JScrollPane jScrollPane2;
-    private javax.swing.JScrollPane jScrollPane3;
     private javax.swing.JDesktopPane panelEscritorio;
     private javax.swing.JSeparator separador1;
-    private javax.swing.JTextArea textoAreaActivos;
-    private javax.swing.JTextArea textoAreaEspecial;
-    private javax.swing.JTextArea textoAreaOrden;
     // End of variables declaration//GEN-END:variables
 }
